77a5bafc0e9ee1feb9434b56a91243c934c4e070
Dan <dpedersen84@gmail.com>
2018-06-18 12:46:59 -0500

    buttons and json on main index.html

--- public/app.js
+++ public/app.js
@@ -1,10 +1,53 @@
 $.getJSON("/articles", function(data) {
     console.log(data);
 
-    data.forEach(element => {
-        // console.log(element);
+    data.forEach(article => {
+        // console.log(article);
+        let favBtn = $("<button>")
+                        .addClass("favorite")
+                        .text("Favorite Article");
 
-        $("")
+        let delFavBtn = $("<button>")
+                        .addClass("delete")
+                        .text("Remove Favorite");
+
+        let noteBtn = $("<button>")
+                        .addClass("note")
+                        .text("Add Note");
+            
+        let viewBtn = $("<button>")
+                        .addClass("view")
+                        .text("View Article")
+                        .attr("href", article.link);
+
+        let div = $("<div>")
+                
+        if (article.favorite === true) {
+            $("#favorites").append(
+                // $("<div>").text(article._id),
+                $("<div>").text(article.title),
+                $("<div>").text(article.link),
+                $("<div>").html(favBtn)
+                .append(delFavBtn)
+                .append(noteBtn)
+                .append(viewBtn)
+                
+            )
+        }
+        else {
+            $("#articles").append(
+                // $("<div>").text(article._id),
+                $("<div>").text(article.title),
+                $("<div>").text(article.link),
+                $("<div>").html(favBtn)
+                // .append(delFavBtn)
+                // .append(noteBtn)
+                .append(viewBtn)
+                
+            )
+            
+        }
+
     });
 });
 
